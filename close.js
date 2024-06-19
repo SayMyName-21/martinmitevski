3d255c1258c28651a575d32c4530a052fd246667
SayMyName-21 <lolmonkeywrench101@gmail.com>
2024-06-19 22:19:20 +0200

    Add files via upload
    files, haha. haha. ha.

---
+++ close.js
@@ -0,0 +1,11 @@
+function wap() {
+    document.getElementById("open1").classList.toggle("show");
+}
+function dap() {
+    document.getElementById("open3").classList.toggle("show");
+
+}
+function gap() {
+    document.getElementById("open2").classList.toggle("show");
+
+}
